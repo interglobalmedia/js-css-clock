e549ac144d7128cf907c383a75de1cc5d3948f6a
interglobalmedia <interglobalmedia@gmail.com>
2016-12-17 01:36:38 -0500

    Removed if statement fix in clock.js because removed transition: all 0.03s in styles.css. Would have had to do a complete transformation of the code. Will try that another time. Also added the fix to the 1 hour time difference (1 hour behind) in clock.js. Have no clue why that happened all of a sudden.

--- scripts/clock.js
+++ scripts/clock.js
@@ -4,6 +4,8 @@ const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     // get time
     const now = new Date();
+    /* Had to compensate for difference of one hour. It was originally one hour behind. Why I have no clue. */
+    now.setTime(now.getTime() - new Date().getTimezoneOffset() / 60 / 1000);
     const seconds = now.getSeconds();
     // 360 degrees
     const secondsDegrees = ((seconds / 60) * 360) + 90;
@@ -16,8 +18,8 @@ function setDate() {
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     // because this does not work properly in Safari or FireFox
+    minHand.style.mozTransform = `rotate(${minutesDegrees}deg)`;
     minHand.style.webkitTransform = `rotate(${minutesDegrees}deg)`;
-    minHand.sthyle.mozTransform = `rotate(${minutesDegrees}deg)`;
     minHand.style.transform = `rotate(${minutesDegrees}deg)`;
     console.log(minutes);
 
@@ -28,11 +30,6 @@ function setDate() {
     hourHand.style.webkitTransform = `rotate(${hoursDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
     console.log(hours);
-    /*
-    We need to make sure that the minute hand moves when the second hand hits the 12 o'clock position. Before we move the minute hand, we need to communicate how far into the current minute we are. This condition checks to see if the hand is the minutes hand, and if so, sets a data attribute with the current degree of the seconds hand. This fixes the tendency for the second hand to jump several seconds when crossing the 12 mark. It smooths the rotation of the second hand in relation to the rotation of the minute hand.
-    Why the need for this fix in the first place? Because when the clock is first drawn on the screen, there is less than one minute before the minute hand needs to move. To allow for this, we can calculate how long until this first minute ends and manually nudge the hand along. SetInterval takes care of that. And, as mentioned earlier, before we move the minute hand, we need to communicate how far into the current minute we are. */
-    if(minHand === 'minutes') {
-        minHand.parentNode.setAttribute('data-second-deg', secondHand.secondsDegrees);
-    }
+    /* Removed if statement fix because only added it for the sake of the 'transition: all 0.03s'. Since removed it, don't need this anymore. */
 }
 setInterval(setDate, 1000);
