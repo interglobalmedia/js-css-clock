1b565cbd4a29a41d8f24427762443353d872ebdd
interglobalmedia <interglobalmedia@gmail.com>
2017-01-30 09:27:13 -0500

    Changed js for hour hand to ES6. Added string templating.

--- app/assets/scripts/modules/clock.js
+++ app/assets/scripts/modules/clock.js
@@ -2,8 +2,8 @@ const dialLines = document.getElementsByClassName('diallines');
 const clockEl = document.getElementsByClassName('clock-face')[0];
 /* this for loop makes sure that the hour hand moves 6 degrees every minute. the actual diallines that are being added here have a property of visibility: hidden because they are not decessary to the design. */
 for (let i = 1; i < 60; i++) {
-    clockEl.innerHTML += "<div class='diallines'></div>";
-    dialLines[i].style.transform = "rotate(" + 6 * i + "deg)";
+    clockEl.innerHTML += `<div class=diallines></div>`;
+    dialLines[i].style.transform = `rotate(${6 * i}${deg})`;
 }
 export function setDate() {
     // get time
